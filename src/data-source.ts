f7ddaae493a6782884b4f2cca43e9ac11d2cdf47
muuo <anthonymuuo92@gmail.com>
2025-08-17 20:17:09 +0300

    production deployment by including external database from render  also added the revert migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,12 +2,15 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5433,
-  username: 'postgres',
-  database: 'postgres',
-  password: 'pass123',
+  url: process.env.DATABASE_URL,
   entities: [__dirname + '/**/*.entity.{ts,js}'],
   migrationsTableName: 'migrations',
   migrations: [__dirname + '/migrations/**/*.ts'],
+  ssl: { rejectUnauthorized: false },
 });
+
+// host: 'localhost',
+// port: 5433,
+// username: 'postgres',
+// database: 'postgres',
+// password: 'pass123',
